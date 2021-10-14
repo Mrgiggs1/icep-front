82f9e7119d38440ebfeb2ccb7d1d14a3124f1b8c
ReneilweSesoko <reneilwe.sesoko>
2021-10-14 15:53:39 +0200

    edit on admin

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { AppService } from '../app.service';
 
 @Component({
@@ -10,6 +11,7 @@ export class UserProfileComponent implements OnInit {
 
   adminDetails: any;
   token: any;
+  editForm: boolean = false;
 
   constructor(public appService: AppService) { }
 
@@ -22,5 +24,14 @@ export class UserProfileComponent implements OnInit {
       }, error => {
         console.log(error , 'GET admin profile error!!!')
     });
+  }
+
+  openEditForm(){
+    
+    this.editForm = !this.editForm
+  }
+
+  onSubmit(editForm: NgForm){
+    
   }
 }
